3cc0bd9fd62827e2d4e88f639a014ea7bfb0d18e
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-24 18:29:50 +0900

    test: 5글자를 초과하지 않는 이름 생성 테스트

--- src/test/java/racingcar/model/vo/NameTest.java
+++ src/test/java/racingcar/model/vo/NameTest.java
@@ -2,6 +2,7 @@ package racingcar.model.vo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +10,34 @@ class NameTest {
 
     @DisplayName("입력 이름이 비어있을 때 예외 테스트")
     @Test
-    void NameEmptyExceptionTest() {
+    void nameEmptyExceptionTest() {
         //given
         String input = "";
 
         //when //then
         assertThrows(IllegalArgumentException.class, () -> new Name(input));
+    }
+
+    @DisplayName("5글자를 초과한 이름 입력 시 예외 테스트")
+    @Test
+    void createNameMaxLengthExceed() {
+        //given
+        String name = "McGregor";
+
+        //when //then
+        assertThrows(IllegalArgumentException.class, () -> new Name(name));
+    }
+
+    @DisplayName("5글자를 초과하지 않은 이름 입력 테스트")
+    @Test
+    void createNameWithnMaxLength() {
+        //given
+        String input = "woni";
+
+        //when
+        Name name = new Name(input);
+        // then
+        Assertions.assertThat(name.getName())
+                .isEqualTo(input);
     }
 }
