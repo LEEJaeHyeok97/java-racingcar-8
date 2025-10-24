c605c3e1ca6910c5ae4f84a9bb322ccc1981c00c
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-24 19:04:06 +0900

    경주 시도 횟수 예외 처리 기능 추가

--- src/main/java/racingcar/model/vo/RacingCount.java
+++ src/main/java/racingcar/model/vo/RacingCount.java
@@ -2,9 +2,41 @@ package racingcar.model.vo;
 
 public class RacingCount {
 
+    public static final int MIN_RACE_COUNT = 1;
     private final int count;
 
     public RacingCount(int count) {
         this.count = count;
+    }
+
+    public static RacingCount from(String racingCount) {
+        int parsedLong = getParsedLong(racingCount);
+        validateMinRaceCount(parsedLong);
+        return new RacingCount(parsedLong);
+    }
+
+    private static void validateMinRaceCount(int parsedLong) {
+        if (parsedLong < MIN_RACE_COUNT) {
+            throw new IllegalArgumentException("경주 시도 횟수는 1 이상의 정수로 입력해야 합니다.");
+        }
+    }
+
+    private static int getParsedLong(String racingCount) {
+        validateIsEmpty(racingCount);
+        return parseInteger(racingCount);
+    }
+
+    private static int parseInteger(String racingCount) {
+        try {
+            return Integer.parseInt(racingCount);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("경주 시도 횟수에 문자를 입력할 수 없습니다.");
+        }
+    }
+
+    private static void validateIsEmpty(String racingCount) {
+        if (racingCount.isEmpty()) {
+            throw new IllegalArgumentException("경주 시도 횟수는 공백이 될 수 없습니다.");
+        }
     }
 }
