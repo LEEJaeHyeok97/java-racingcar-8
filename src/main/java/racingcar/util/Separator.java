949a8167fe8b3d7c2f47f037d36cff794dc5d9ce
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-24 16:39:20 +0900

    feat: 이름 구분자가 연속으로 들어왔을 경우 예외처리

--- src/main/java/racingcar/util/Separator.java
+++ src/main/java/racingcar/util/Separator.java
@@ -1,10 +1,28 @@
 package racingcar.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Separator {
 
-    public static final String DELIMITER = ",";
+    public static final String CONTINOUS_DELIMITER_REGEX = ".*,{2,}.*";
+    public static final Pattern CONTINUOUS_DELIMITER_PATTERN = Pattern.compile(CONTINOUS_DELIMITER_REGEX);
 
     public String[] splitNames(String input) {
-        return input.split(DELIMITER);
+        validateContinuousDelimiter(input);
+
+        return input.split(input);
+    }
+
+    private void validateContinuousDelimiter(String input) {
+        Matcher matcher = getContinuousDelimiterMatcher(input);
+
+        if (matcher.matches()) {
+            throw new IllegalArgumentException("구분자가 연속으로 입력되었습니다.");
+        }
+    }
+
+    private Matcher getContinuousDelimiterMatcher(String input) {
+        return CONTINUOUS_DELIMITER_PATTERN.matcher(input);
     }
 }
