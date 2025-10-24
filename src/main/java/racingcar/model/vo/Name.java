4fd4321e291aa1bdc6126d1dd1f7eca19ae165d8
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-24 18:06:57 +0900

    feat: 자동차의 이름 입력 검증기능 구현

--- src/main/java/racingcar/model/vo/Name.java
+++ src/main/java/racingcar/model/vo/Name.java
@@ -2,10 +2,29 @@ package racingcar.model.vo;
 
 public class Name {
 
+    public static final int MAX_LENGTH_LIMIT = 5;
     private final String name;
 
     public Name(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        validateisEmpty(name);
+        validateIsMaxLength(name);
+    }
+
+    private static void validateIsMaxLength(String name) {
+        if (name.length() > MAX_LENGTH_LIMIT) {
+            throw new IllegalArgumentException("자동차의 이름의 길이는 5를 초과할 수 없습니다.");
+        }
+    }
+
+    private void validateisEmpty(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("자동차의 이름은 공백이 될 수 없습니다.");
+        }
     }
 
     public String getName() {
