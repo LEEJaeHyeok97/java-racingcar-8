80c0ea95c31ae1aa4ca76de6e1216f463e7679b0
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-24 19:33:53 +0900

    test: 유효하지 않은 값이 경주 시도 입력에 입력된 경우 테스트

--- src/test/java/racingcar/model/vo/RacingCountTest.java
+++ src/test/java/racingcar/model/vo/RacingCountTest.java
@@ -7,11 +7,33 @@ import org.junit.jupiter.api.Test;
 
 class RacingCountTest {
 
+    public static final String INVALID_MIN_COUNT = "0";
+
     @DisplayName("경주 시도 횟수에 공백이 입력된 경우 예외 테스트")
     @Test
     void racingCountEmptyExceptionTest() {
         //given
         String input = "";
+
+        //when //then
+        assertThrows(IllegalArgumentException.class, () -> RacingCount.from(input));
+    }
+
+    @DisplayName("최소 경주 시도 횟수 이하의 값이 입력된 경우 예외 테스트")
+    @Test
+    void racingCountMaxLengthLimitExceedExceptionTest() {
+        //given
+        String input = INVALID_MIN_COUNT;
+
+        //when //then
+        assertThrows(IllegalArgumentException.class, () -> RacingCount.from(input));
+    }
+
+    @DisplayName("숫자가 아닌 값이 경주 시도 횟수에 입력되면 예외를 발생한다.")
+    @Test
+    void racingCountNonNumericInput() {
+        //given
+        String input = "javajigi";
 
         //when //then
         assertThrows(IllegalArgumentException.class, () -> RacingCount.from(input));
