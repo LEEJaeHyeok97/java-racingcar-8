5d5ea281a683b50fc344425243c4f35679e032b9
LeeJaeHyeok97 <hazardous10@naver.com>
2025-10-27 01:05:16 +0900

    test: 랜덤 숫자가 4 이하일 때 자동차 이동을 검증하는 테스트 추가

--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -2,7 +2,6 @@ package racingcar.model;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import racingcar.util.NumberGenerator;
@@ -13,16 +12,30 @@ class CarTest {
     @ParameterizedTest
     @CsvSource(value = {"4", "5", "6", "7", "8", "9"})
     @DisplayName("랜덤 숫자가 4 이상일 경우 자동차는 한 칸 전진한다.")
-    @Test
-    void carMovesWhenRandomNumberIsFourOrMore() {
+    void carMovesWhenRandomNumberIsFourOrMore(int number) {
         //given
         Car car = Car.of("pobi");
-        NumberGenerator generator = new FixedNumberGenerator(4);
+        NumberGenerator generator = new FixedNumberGenerator(number);
 
         //when
         car.move(generator.generateNumber());
 
         //then
         Assertions.assertThat(car.getCurrentPosition()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0", "1", "2", "3"})
+    @DisplayName("랜덤 숫자가 3 이하일 경우 자동차는 움직이지 않는다.")
+    void carDoesNotMoveWhenRandomNumberIsLessThanFour(int number) {
+        //given
+        Car car = Car.of("pobi");
+        NumberGenerator generator = new FixedNumberGenerator(number);
+
+        //when
+        car.move(generator.generateNumber());
+
+        //then
+        Assertions.assertThat(car.getCurrentPosition()).isEqualTo(0);
     }
 }
